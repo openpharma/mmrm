928e96fd996e0116921c4b5d7b4b644aea1c7c16
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2022-04-28 10:14:26 +0200

    additional cleanups

---
+++ R/package.R
@@ -0,0 +1,9 @@
+#' `mmrm` Package
+#'
+#' `mmrm` implements mixed models with repeated measures (MMRM) in R.
+#'
+"_PACKAGE"
+
+#' @import checkmate
+#' @importFrom glmmTMB addForm
+NULL
--- tests/testthat/test-hello.R
+++
@@ -1,5 +0,0 @@
-test_that("hello greets the entity", {
-  result <- hello("foo")
-  expected <- "Hello, Foo"
-  expect_identical(result, expected)
-})
