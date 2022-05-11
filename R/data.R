a9b4972fad19a22a09c04b3cfcb619e475840802
Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
2022-05-11 15:06:26 +0200

    3: Design for MMRM fit (#8)
    * add example data, start design doc
    
    * build_formula
    
    * all except lsmeans computations
    
    * some progress towards emmeans
    
    * try without luck to fix the Satterthwaite one-dimensional case
    
    * progress up to df_md()
    
    * continue with emmeans prep
    
    * complete emmeans
    
    * fix pkgdown

---
+++ R/data.R
@@ -0,0 +1,21 @@
+# fev_data ----
+
+#' Example Data on FEV1
+#'
+#' @description `r lifecycle::badge("experimental")`
+#'
+#' @note Measurements of FEV1 (forced expired volume in one second) is a measure
+#' of how quickly the lungs can be emptied. Low levels of FEV1 may indicate
+#' chronic obstructive pulmonary disease (COPD).
+#'
+#' @format A `tibble` with 800 rows and 7 variables:
+#'   - `USUBJID`: subject ID.
+#'   - `AVISIT`: visit number.
+#'   - `ARMCD`: treatment, `TRT` or `PBO`.
+#'   - `RACE`: 3-category race.
+#'   - `SEX`: sex.
+#'   - `FEV1_BL`: FEV1 at baseline (%).
+#'   - `FEV1`: FEV1 at study visits.
+#'
+#' @source This is an artificial dataset.
+"fev_data"
