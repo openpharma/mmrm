2cba7ad7f5d3c54276da36a240b9ec0f583cb96a
juliadedic1 <julia.dedic@roche.com>
2022-05-16 18:21:00 -0400

    added additional unittests

--- tests/testthat/test-formula.R
+++ tests/testthat/test-formula.R
@@ -6,7 +6,23 @@ test_that("h_build_formula gives error message if incorrect covariance structure
 })
 
 
-test_that("h_build_formula gives no message if input is correct", {
+test_that("h_build_formula builds the correct formula", {
   vars <- list(response = "AVAL", id = "USUBJID", arm = "ARMCD", visit = "AVISIT", covariates = c("RACE", "SEX"))
-  expect_silent(h_build_formula(vars, "compound-symmetry"))
+  result <- h_build_formula(vars, "compound-symmetry")
+  expected <- AVAL ~ RACE + SEX + ARMCD * AVISIT + cs(0 + AVISIT | USUBJID)
+  expect_identical(result, expected)
+})
+
+test_that("h_build_formula gives builds the correct formula with no arm variable given", {
+  vars <- list(response = "AVAL", id = "USUBJID", visit = "AVISIT", covariates = c("RACE", "SEX"))
+  result <- h_build_formula(vars, "compound-symmetry")
+  expected <- AVAL ~ RACE + SEX +  AVISIT + cs(0 + AVISIT | USUBJID)
+  expect_identical(result, expected)
+})
+
+test_that("h_build_formula gives builds the correct formula with no covariates given", {
+  vars <- list(response = "AVAL", id = "USUBJID",arm = "ARMCD", visit = "AVISIT")
+  result <- h_build_formula(vars, "compound-symmetry")
+  expected <- AVAL ~ ARMCD * AVISIT + cs(0 + AVISIT | USUBJID)
+  expect_identical(result, expected)
 })
