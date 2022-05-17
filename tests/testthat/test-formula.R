f3e3e114f09babbf0cab4e75cc13109f321a9352
juliadedic1 <99106549+juliadedic1@users.noreply.github.com>
2022-05-17 11:27:45 -0400

    Update tests/testthat/test-formula.R
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>

--- tests/testthat/test-formula.R
+++ tests/testthat/test-formula.R
@@ -21,7 +21,7 @@ test_that("h_build_formula builds the correct formula with no arm variable given
 })
 
 test_that("h_build_formula builds the correct formula with no covariates given", {
-  vars <- list(response = "AVAL", id = "USUBJID",arm = "ARMCD", visit = "AVISIT")
+  vars <- list(response = "AVAL", id = "USUBJID", arm = "ARMCD", visit = "AVISIT")
   result <- as.character(h_build_formula(vars, "compound-symmetry"))
   expected <- as.character(AVAL ~ ARMCD * AVISIT + cs(0 + AVISIT | USUBJID))
   expect_identical(result, expected)
