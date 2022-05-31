2f95787f7560aa718f8dc3eb2b10a5044b7963bb
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2022-05-31 20:14:44 +0200

    include expected covariance numbers

--- tests/testthat/test-covariance.R
+++ tests/testthat/test-covariance.R
@@ -3,7 +3,7 @@ test_that("h_cov_estimate works as expected", {
     MYID = as.factor(c(1, 1, 1, 2, 2, 3, 3)),
     AVISIT = as.factor(c(1, 2, 2, 1, 2, 1, 2)),
     RESP = c(20, 30, 40, 50, 60, 70, 80)
-    )
+  )
   model <- glmmTMB::glmmTMB(
     RESP ~ ar1(0 + AVISIT | MYID),
     data = data,
@@ -12,6 +12,21 @@ test_that("h_cov_estimate works as expected", {
   )
   class(model) <- c("mmrm_fit", "glmmTMB")
   result <- h_cov_estimate(model)
-  expected <- structure(glmmTMB::VarCorr(model)$cond[[1L]], id = "2", n_parameters = as.integer(2))
-  expect_identical(result, expected)
+  expected <- structure(
+    matrix(
+      c(465.89, 190.54, 190.54, 465.89),
+      nrow = 2L, ncol = 2L,
+      dimnames = list(c("AVISIT1", "AVISIT2"), c("AVISIT1", "AVISIT2"))
+    ),
+    stddev = c(AVISIT1 = 21.58, AVISIT2 = 21.58),
+    correlation = matrix(
+      c(1, 0.409, 0.409, 1),
+      nrow = 2L, ncol = 2L,
+      dimnames = list(c("AVISIT1", "AVISIT2"), c("AVISIT1", "AVISIT2"))
+    ),
+    blockCode = c(ar1 = 3),
+    id = "2",
+    n_parameters = 2L
+  )
+  expect_equal(result, expected, tolerance = 1e-3)
 })
