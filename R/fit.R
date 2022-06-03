ca4aaf9bbbd7caa4972f7475ab8932abcddbfc7d
juliadedic1 <99106549+juliadedic1@users.noreply.github.com>
2022-06-03 14:47:41 -0400

    adding fit_single_optimizer function. Unit tests are not fully workin… (#23)
    * adding fit_single_optimizer function. Unit tests are not fully working yet.
    
    * updated unit tests. Not sure if the number of tests is overkill
    
    * fixed unit tests
    
    * Update R/fit.R
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * fixed lintr issue
    
    * fix roxygen issue
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>

---
+++ R/fit.R
@@ -0,0 +1,64 @@
+#' Fitting `glmmTMB` Model
+#'
+#' This function helps to fit a `glmmTMB` model with a single optimizer, while capturing messages and warnings.
+#'
+#' @param formula (`formula`)\cr the `glmmTMB` formula.
+#' @param data (`data`)\cr the data to be used for the model.
+#' @param start (`list`)\cr a list of starting values.
+#' @param optimizer (`character`)\cr optimizer to be used to generate the model.
+#'
+#' @return the fitted glmmTMB object, with additional attributes containing warnings,
+#' messages, errors, optimizer used and convergence status.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' mod_fit <- fit_single_optimizer(
+#' formula = h_build_formula(vs),
+#' data = dat,
+#' optimizer = "BFGS"
+#' )
+#' attr(mod_fit, "converged")
+#' }
+fit_single_optimizer <- function(formula,
+                                 data,
+                                 start = NULL,
+                                 optimizer = c("L-BFGS-B", "BFGS", "CG", "nlminb")) {
+  assert_formula(formula)
+  assert_data_frame(data)
+  assert_list(start, null.ok = TRUE)
+  optimizer <- match.arg(optimizer)
+  control <- glmmTMB::glmmTMBControl(
+    optimizer = if (optimizer == "nlminb") stats::nlminb else stats::optim,
+    optArgs = if (optimizer == "nlminb") list() else list(method = optimizer),
+    parallel = 1L
+  )
+  quiet_fit <- h_record_all_output(
+    glmmTMB::glmmTMB(
+      formula = formula,
+      data = data,
+      dispformula = ~0,
+      REML = TRUE,
+      start = start,
+      control = control
+    ),
+    remove = list(
+      warnings = c(
+        "OpenMP not supported.",
+        "'giveCsparse' has been deprecated; setting 'repr = \"T\"' for you"
+      )
+    )
+  )
+  converged <- (length(quiet_fit$warnings) == 0L) &&
+    (length(quiet_fit$errors) == 0L) &&
+    (quiet_fit$result$fit$convergence == 0)
+  structure(
+    quiet_fit$result,
+    errors = quiet_fit$errors,
+    warnings = quiet_fit$warnings,
+    messages = quiet_fit$messages,
+    optimizer = optimizer,
+    converged = converged,
+    class = c("mmrm_fit", class(quiet_fit$result))
+  )
+}
---
+++ tests/testthat/test-fit.R
@@ -0,0 +1,63 @@
+test_that("fit_single_optimizer works as expected with optimizer inputted but no starting values", {
+  dat <- mmrm::fev_data
+  form <- FEV1 ~ ar1(0 + AVISIT | USUBJID)
+  result <- fit_single_optimizer(
+    formula = form,
+    data = dat,
+    optimizer = "BFGS"
+  )
+  expect_identical(class(result), c("mmrm_fit", "glmmTMB"))
+  expect_identical(attr(result, "optimizer"), "BFGS")
+  expect_identical(attr(result, "errors"), NULL)
+  expect_identical(attr(result, "messages"), NULL)
+  expect_identical(attr(result, "warnings"), NULL)
+  expect_true(attr(result, "converged"))
+})
+
+test_that("fit_single_optimizer works as expected with starting values and optimizer inputted", {
+  dat <- mmrm::fev_data
+  form <- FEV1 ~ ar1(0 + AVISIT | USUBJID)
+  result <- fit_single_optimizer(
+    formula = form,
+    data = dat,
+    optimizer = "BFGS",
+    start = list(theta = c(3, 0.5))
+  )
+  expect_identical(class(result), c("mmrm_fit", "glmmTMB"))
+  expect_identical(attr(result, "optimizer"), "BFGS")
+  expect_identical(attr(result, "errors"), NULL)
+  expect_identical(attr(result, "messages"), NULL)
+  expect_identical(attr(result, "warnings"), NULL)
+  expect_true(attr(result, "converged"))
+})
+
+test_that("fit_single_optimizer works as expected with no starting values or optimizer inputted", {
+  dat <- mmrm::fev_data
+  form <- FEV1 ~ ar1(0 + AVISIT | USUBJID)
+  result <- fit_single_optimizer(
+      formula = form,
+      data = dat
+    )
+  expect_identical(class(result), c("mmrm_fit", "glmmTMB"))
+  expect_identical(attr(result, "optimizer"), "L-BFGS-B")
+  expect_identical(attr(result, "errors"), NULL)
+  expect_identical(attr(result, "messages"), NULL)
+  expect_identical(attr(result, "warnings"), NULL)
+  expect_true(attr(result, "converged"))
+})
+
+test_that("fit_single_optimizer works as expected with starting values but no optimizer inputted", {
+  dat <- mmrm::fev_data
+  form <- FEV1 ~ ar1(0 + AVISIT | USUBJID)
+  result <- fit_single_optimizer(
+      formula = form,
+      data = dat,
+      start = list(theta = c(3, 0.5))
+    )
+  expect_identical(class(result), c("mmrm_fit", "glmmTMB"))
+  expect_identical(attr(result, "optimizer"), "L-BFGS-B")
+  expect_identical(attr(result, "errors"), NULL)
+  expect_identical(attr(result, "messages"), NULL)
+  expect_identical(attr(result, "warnings"), NULL)
+  expect_true(attr(result, "converged"))
+})
