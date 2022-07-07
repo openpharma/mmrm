e9a0e98545fdd13823b478df329aa8fd07da77d8
BrianLang <brianlang89@gmail.com>
2022-07-07 21:55:56 +0200

    updated documentation for feedback updated testing for feedback

--- R/component.R
+++ R/component.R
@@ -1,32 +1,32 @@
-#' Component getter for \code{mmrm_tmb} objects
+#' Component Access for `mmrm_tmb` Objects
 #'
-#' @aliases component
-#' @param object a \code{mmrm_tmb} object
-#' @param name of the component to be retrieved
-#' @param \dots ignored, for method compatibility
+#' @description `r lifecycle::badge("experimental")`
+#'
+#' @param object (`mmrm_tmb`)\cr the fitted MMRM.
+#' @param name (`character`)\cr the component(s) to be retrieved.
 #'
 #' @details Available `component()` names are as follows:
-#' - 'call': low-level function call which generated the model.
-#' - 'formula': model formula.
-#' - 'dataset': data set name.
-#' - 'cov_type': covariance structure type.
-#' - 'n_theta': number of parameters.
-#' - 'n_subjects': number of subjects.
-#' - 'n_timepoints': number of modeled time points.
-#' - 'n_obs': total number of observations.
-#' - 'reml': was REML used (ML was used if \code{FALSE}).
-#' - 'neg_log_lik': negative log likelihood.
-#' - 'convergence': convergence code from optimizer.
-#' - 'conv_message': message accompanying the convergence code.
-#' - 'evaluations': number of function evaluations for optimization.
-#' - 'vcov': estimated variance-covariance matrix of coefficients.
-#' - 'varcor': estimated covariance matrix for residuals.
-#' - 'theta_est': estimated variance parameters.
-#' - 'beta_est': estimated coefficients.
-#' - 'theta_vcov':  estimated variance-covariance matrix of variance parameters.
-#' - 'x_matrix': design matrix used.
-#' - 'y_vector': response vector used.
-#' - 'jac_list': Jacobian, see  [h_jac_list()] for details.
+#' - `call`: low-level function call which generated the model.
+#' - `formula`: model formula.
+#' - `dataset`: data set name.
+#' - `cov_type`: covariance structure type.
+#' - `n_theta`: number of parameters.
+#' - `n_subjects`: number of subjects.
+#' - `n_timepoints`: number of modeled time points.
+#' - `n_obs`: total number of observations.
+#' - `reml`: was REML used (ML was used if `FALSE`).
+#' - `neg_log_lik`: negative log likelihood.
+#' - `convergence`: convergence code from optimizer.
+#' - `conv_message`: message accompanying the convergence code.
+#' - `evaluations`: number of function evaluations for optimization.
+#' - `vcov`: estimated variance-covariance matrix of coefficients.
+#' - `varcor`: estimated covariance matrix for residuals.
+#' - `theta_est`: estimated variance parameters.
+#' - `beta_est`: estimated coefficients.
+#' - `theta_vcov`:  estimated variance-covariance matrix of variance parameters.
+#' - `x_matrix`: design matrix used.
+#' - `y_vector`: response vector used.
+#' - `jac_list`: Jacobian, see  [h_jac_list()] for details.
 #'
 #' @seealso \code{\link[lme4]{getME.merMod}}
 #'
@@ -41,8 +41,7 @@ component <- function(object,
                         "conv_message", "call", "theta_est",
                         "beta_est", "x_matrix", "y_vector", "neg_log_lik",
                         "jac_list", "theta_vcov"
-                      ),
-                      ...) {
+                      )) {
   assert_class(object, "mmrm_tmb")
   name <- match.arg(name, several.ok = TRUE)
 
@@ -51,20 +50,20 @@ component <- function(object,
     X = name,
     FUN = switch,
     "call" = object$call,
-    # Strings
+    # Strings.
     "cov_type" = object$formula_parts$cov_type,
     "formula" = deparse(object$call$formula),
     "dataset" = object$call$data,
     "reml" = object$reml,
     "conv_message" = object$opt_details$message,
-    # Numeric of length 1
+    # Numeric of length 1.
     "convergence" = object$opt_details$convergence,
     "neg_log_lik" = object$neg_log_lik,
     "n_theta" = length(object$theta_est),
     "n_subjects" = object$tmb_data$n_subjects,
     "n_timepoints" = object$tmb_data$n_visits,
     "n_obs" = length(object$tmb_data$y_vector),
-    # Numeric of length > 1
+    # Numeric of length > 1.
     "evaluations" = unlist(ifelse(is.null(object$opt_details$evaluations),
                                   list(object$opt_details$counts),
                                   list(object$opt_details$evaluations)
@@ -73,17 +72,18 @@ component <- function(object,
     "theta_est" = object$theta_est,
     "y_vector" = object$tmb_data$y_vector,
     "jac_list" = object$jac_list,
-    # Matrices
+    # Matrices.
     "vcov" = object$beta_vcov,
     "varcor" = object$cov,
     "x_matrix" = object$tmb_data$x_matrix,
     "theta_vcov" = object$theta_vcov,
-    # If not found
+    # If not found.
     "..foo.." =
       stop(sprintf(
-        "component '%s' is not available for class \"%s\"",
+        "component '%s' is not available",
         name, paste0(class(object), collapse = ", ")
-      )), simplify = FALSE
+      )),
+    simplify = FALSE
   )
 
   if (length(name) == 1) list_components[[1]] else list_components
--- tests/testthat/test-component.R
+++ tests/testthat/test-component.R
@@ -3,7 +3,6 @@
 
 test_that("component works as expected", {
   object_mmrm_tmb <- get_mmrm_tmb()
-  object_mmrm <- get_mmrm()
 
   expect_equal(names(component(object_mmrm_tmb)),
                c("cov_type", "n_theta", "n_subjects", "n_timepoints",
@@ -12,22 +11,15 @@ test_that("component works as expected", {
                  "conv_message", "call", "theta_est",
                  "beta_est", "x_matrix", "y_vector", "neg_log_lik",
                  "jac_list", "theta_vcov"))
-  expect_equal(names(component(object_mmrm)),
-               c("cov_type", "n_theta", "n_subjects", "n_timepoints",
-                 "n_obs", "vcov", "varcor", "formula", "dataset",
-                 "reml", "convergence", "evaluations",
-                 "conv_message", "call",  "theta_est",
-                 "beta_est", "x_matrix", "y_vector", "neg_log_lik",
-                 "jac_list", "theta_vcov"))
 
   expect_numeric(component(object_mmrm_tmb, "n_theta"), lower = 0)
 
-  expect_list(component(object_mmrm_tmb, c("n_theta", "NULL", "x_matrix")),
-              len = 2)
-  expect_list(component(object_mmrm_tmb, c("n_theta", "NULL", "x_matrix", "varcor")),
-              len = 3)
-  expect_named(component(object_mmrm_tmb, c("n_theta", "NULL", "x_matrix", "varcor")),
-               c("n_theta", "x_matrix", "varcor"))
+  test_list <- component(object_mmrm_tmb,
+                name = c("n_theta", "NULL", "x_matrix", "varcor"))
 
-  expect_identical(component(object_mmrm, "varcor"), component(object_mmrm)$varcor)
+  expect_list(test_list, len = 3)
+
+  expect_named(test_list, c("n_theta", "x_matrix", "varcor"))
+
+  expect_identical(component(object_mmrm_tmb, "varcor"), component(object_mmrm_tmb)$varcor)
 })
