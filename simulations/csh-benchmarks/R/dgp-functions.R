3720f52c6990a00d8b879b64c9d97c4f0382cce9
PhilBoileau <boileap2@gene.com>
2022-11-28 16:56:04 -0800

    fixed definition of random effects model in dgp

--- simulations/csh-benchmarks/R/dgp-functions.R
+++ simulations/csh-benchmarks/R/dgp-functions.R
@@ -2,49 +2,63 @@
 ## Data-Generating Processes
 ################################################################################
 
-balanced_dgp_fun <- function(
+#' A Simple RCT Data-Generating Function
+#'
+#' @description This function simulates a randomized control trial with
+#'   participants equally split across the control and treatment arms.
+#'   Participants' repeated measures are assumed to be normally distributed with
+#'   heterogeneous compound symmetry covariance matrix. Participants' outcomes
+#'   are collected at each time point.
+#'
+#' @param num_part A numeric indicating the total number of participants.
+#' @param num_rep_meas A numeric indicating the number of repeated measures.
+#' @param fixed_int A numeric corresponding to the fixed intercept effect.
+#' @param fixed_trt A numeric corresponding to the fixed treatment effect.
+#' @param fixed_time A numeric corresponding to the fixed time point effect.
+#' @param fixed_time_trt A numeric corresponding to the fixed time x treatment
+#'   interaction effect.
+#' @param fixed_base_cov A numeric corresponding to the a baseline covariate's
+#'   fixed effect.
+#' @param outcome_vars A numeric vector with length equal to num_rep_meas. It is
+#'   the vector of the repeated measures' variances.
+#' @param outcome_cor A numeric corresponding to the correlation parameter in
+#'   the heterogeneous compound symmetry covariance matrix.
+#'
+#' @return A list containing the simulated participants' IDs, time point
+#'   indicators, outcomes, treatment assignments and baseline covariates.
+
+rct_dgp_fun <- function(
   num_part = 100,
-  n_rep_meas = 5,
+  num_rep_meas = 5,
   fixed_int = 1,
   fixed_trt = 1,
   fixed_time = 1,
-  fixed_time_treat = 1,
-  fixed_base_cov_1 = 1,
-  fixed_base_cov_2 = 1,
-  part_effect_var = 1,
-  outcome_vars = rep(1, n_rep_meas),
+  fixed_time_trt = 1,
+  fixed_base_cov = 1,
+  outcome_vars = rep(1, num_rep_meas),
   outcome_cor = 0.5
 ) {
 
   ## form a balanced data.frame
   cov_df <- data.frame(
     participant = seq_len(num_part),
-    base_cov_1 = rnorm(num_part),
-    base_cov_2 = rnorm(num_part),
+    base_cov = rbinom(num_part, 1, 0.7),
     trt = c(rep(0, round(num_part / 2)), rep(1, num_part - round(num_part / 2)))
   )
   time_point_df <- expand.grid(
-    time = seq_len(n_rep_meas),
+    time = seq_len(num_rep_meas),
     participant = seq_len(num_part)
   )
   df <- cov_df %>% left_join(time_point_df, by = "participant")
 
   ## produce the model matrix for the fixed effects
   fixed_model_mat <- model.matrix(
-    ~ base_cov_1 + base_cov_2 + trt + time + trt:time, data = df
+    ~ base_cov + trt*time, data = df
   )
 
   ## define the full model formula
   full_formula <-
-    "y ~ base_cov_1 + base_cov_2 + trt + time + trt:time + (1 | participant)"
-
-  ## produce the random effects model matrix
-  df$y <- 1
-  rand_model_mat <- lme4::lFormula(eval(full_formula), df)
-  rand_model_mat <- t(as.matrix(rand_model_mat$reTrms$Zt))
-
-  ## simulate the random intercept for each participant
-  u <- rnorm(num_part, 0, part_effect_var)
+    "y ~ base_cov + trt*time + (time | participant)"
 
   ## define the repeated measures correlation structure, assuming
   ## heterogeneous compound symmetry
@@ -53,20 +67,17 @@ balanced_dgp_fun <- function(
 
   ## generate the outcomes
   beta <- c(
-    fixed_int, fixed_base_cov_1, fixed_base_cov_2, fixed_trt, fixed_time,
-    fixed_time_treat
+    fixed_int, fixed_base_cov, fixed_trt, fixed_time, fixed_time_trt
   )
-  df$y <- fixed_model_mat %*% beta + rand_model_mat %*% u +
-    as.vector(t(MASS::mvrnorm(num_part, rep(0, n_rep_meas), csh_mat)))
+  df$y <- fixed_model_mat %*% beta +
+    as.vector(t(MASS::mvrnorm(num_part, rep(0, num_rep_meas), csh_mat)))
 
   ## return the generated data
   return(list(
-    participant = df$participant,
-    time = df$time,
+    participant = as.factor(df$participant),
+    time = as.factor(df$time),
     y = df$y,
     trt = df$trt,
-    base_cov_1 = df$base_cov_1,
-    base_cov_2 = df$base_cov_2
-
+    base_cov = df$base_cov
   ))
 }
