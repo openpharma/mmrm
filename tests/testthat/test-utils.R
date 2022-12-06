8d99b97691c8ee6af82f414a8158d441f7acacb5
Li,Liming <liming.li@roche.com>
2022-12-06 10:22:09 +0800

    put tests in utils

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,3 +1,5 @@
+# h_record_all_outputs ---
+
 test_that("h_record_all_outputs correctly removes specified messages", {
   result <- h_record_all_output({
     x <- 1
@@ -23,4 +25,25 @@ test_that("h_record_all_outputs works as expected with no removal list given for
   expected <- list(result = 3, warnings = "something went wrong", errors = NULL,
   messages = c("O nearly done", "oh noo"))
   expect_identical(result, expected)
+})
+
+
+# trace of matrix ---
+
+test_that("trace of a matrix works as expected", {
+  mx <- matrix(0, nrow = 3, ncol = 4)
+  expect_error(h_tr(mx), "x must be square matrix")
+  v <- c(1, 3, 2)
+  expect_equal(h_tr(diag(v)), 6)
+})
+
+# free_cors ---
+
+test_that("free_cores throws deprecation warning", {
+  skip_on_cran()
+  expect_warning(
+    free_cores(),
+    regexp = "`free_cores()` was deprecated in mmrm 0.1.6.",
+    fixed = TRUE
+  )
 })
