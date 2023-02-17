c355cd9a7db52a4e9a5f77e75b8082701bd63b5c
PhilBoileau <boileap2@gene.com>
2023-02-17 14:46:21 -0800

    data frame assembler for method wrappers

--- simulations/missing-data-benchmarks/tests/testthat.R
+++ simulations/missing-data-benchmarks/tests/testthat.R
@@ -1,2 +1,12 @@
+# load required libraries
+library(simChef)
 library(testthat)
-testthat::test_dir()
+
+# source the R scripts
+sim_functions_files = list.files(
+  c("R/dgp", "R/method", "R/eval", "R/viz"),
+  pattern = "*.R$", full.names = TRUE, ignore.case = TRUE
+)
+sapply(sim_functions_files, source)
+
+test_sim_dir()
