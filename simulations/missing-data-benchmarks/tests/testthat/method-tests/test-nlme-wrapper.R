faadd6b50b72f8643cc0a2c2704e1b53334dcaf1
PhilBoileau <boileap2@gene.com>
2023-02-17 15:28:18 -0800

    nlme wrapper function

---
+++ simulations/missing-data-benchmarks/tests/testthat/method-tests/test-nlme-wrapper.R
@@ -0,0 +1,23 @@
+test_that(paste(
+  "nlme_wrapper_fun produces treatment effect estimates close to ground truth",
+  "in large samples"
+), {
+
+  set.seed(61234)
+  dgp_output <- rct_dgp_fun(
+    n_obs = 10000,
+    outcome_covar_mat = diag(3),
+  )
+  nlme_fit <- nlme_wrapper_fun(
+    participant = dgp_output$participant,
+    visit_num = dgp_output$visit_num,
+    base_bcva = dgp_output$base_bcva,
+    strata = dgp_output$strata,
+    trt = dgp_output$trt,
+    bcva_change = dgp_output$bcva_change,
+    covar_type = "us"
+  )
+
+  trt_effect_est <- as.numeric(coef(nlme_fit$fit)["trt"])
+  expect_equal(trt_effect_est, 0, tolerance = 0.05)
+})
