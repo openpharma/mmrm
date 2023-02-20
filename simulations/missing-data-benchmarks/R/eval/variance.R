8c1772fe872499ef8072a6c6d9a534afd023ddbf
PhilBoileau <boileap2@gene.com>
2023-02-20 15:50:56 -0800

    add variance evaluators

--- simulations/missing-data-benchmarks/R/eval/variance.R
+++ simulations/missing-data-benchmarks/R/eval/variance.R
@@ -1,38 +1,33 @@
-bias_fun <- function(fit_results, true_params) {
+variance_fun <- function(fit_results) {
 
   group_vars <- c(".dgp_name", ".method_name", "n_obs")
   fit_results %>%
     dplyr::mutate(
-      error = purrr::pmap(
-        .l = list(fit, .dgp_name, data),
-        .f = function(f, dgp_name, dt) {
+      estimates = purrr::pmap(
+        .l = list(fit, data),
+        .f = function(f, dt) {
 
           # extract the trt:vist_num estimates
           estimates <- get_trt_visit_num_ests(f, dt)
 
-          # extract the true parameter values
-          true_values <- true_params[[dgp_name]]
-
-          # compute the errors
-          err <- estimates - true_values
-          names(err) <- paste0(
+          names(estimates) <- paste0(
             "trt_visit_num",
             stringr::str_pad(seq_len(length(estimates)), width = 2, pad = "0")
           )
-          err
+          estimates
         }
       )
     ) %>%
-    dplyr::select(dplyr::all_of(group_vars), error) %>%
-    tidyr::unnest_wider(col = error) %>%
+    dplyr::select(dplyr::all_of(group_vars), estimates) %>%
+    tidyr::unnest_wider(col = estimates) %>%
     dplyr::group_by(dplyr::across({{group_vars}})) %>%
     dplyr::summarise(
-      dplyr::across(dplyr::contains("trt_visit_num"), mean),
+      dplyr::across(dplyr::contains("trt_visit_num"), var),
       .groups = "drop"
     ) %>%
     tidyr::pivot_longer(
       cols = dplyr::contains("trt_visit_num"),
       names_to = "coefficient",
-      values_to = "bias"
+      values_to = "variance"
     )
 }
