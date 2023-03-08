3f0e07b447fca2dd1314c85b1976fc55440ca6ba
Liming Li <liming.li@roche.com>
2023-03-08 07:28:39 +0000

    potential update following paper suggest

--- src/empirical.cpp
+++ src/empirical.cpp
@@ -1,9 +1,11 @@
 #include "derivatives.h"
+#include <unsupported/Eigen/MatrixFunctions>
+#include <chrono>
 
 using namespace Rcpp;
 using std::string;
 // Obtain the empirical given beta, beta_vcov, theta.
-List get_empirical(List mmrm_data, NumericVector theta, NumericVector beta, NumericMatrix beta_vcov, bool jackknife) {
+List get_empirical(List mmrm_data, NumericVector theta, NumericVector beta, NumericMatrix beta_vcov, int type) {
   NumericMatrix x = mmrm_data["x_matrix"];
   matrix<double> x_matrix = as_matrix(x);
   NumericVector y = mmrm_data["y_vector"];
@@ -31,6 +33,7 @@ List get_empirical(List mmrm_data, NumericVector theta, NumericVector beta, Nume
   int n_theta = theta.size();
   int theta_size_per_group = n_theta / n_groups;
   int p = x.cols();
+  std::chrono::steady_clock::time_point s1 = std::chrono::steady_clock::now();
   // Use map to hold these base class pointers (can also work for child class objects).
   std::map<int, derivatives_base<double>*> derivatives_by_group;
   for (int r = 0; r < n_groups; r++) {
@@ -42,9 +45,13 @@ List get_empirical(List mmrm_data, NumericVector theta, NumericVector beta, Nume
       derivatives_by_group[r] = new derivatives_nonspatial<double>(vector<double>(theta_v.segment(r * theta_size_per_group, theta_size_per_group)), n_visits, cov_type);
     }
   }
-  matrix<double> meat = matrix<double>::Zero(p, p);  
-  matrix<double> xt_g_simga_inv_chol = matrix<double>::Zero(p, n_observations);
-  matrix<double> ax = matrix<double>::Zero(n_observations, p);
+  std::chrono::steady_clock::time_point s2 = std::chrono::steady_clock::now();
+  matrix<double> meat = matrix<double>::Zero(p, p);
+  matrix<double> hx = x_matrix * beta_vcov_matrix * x_matrix.transpose();
+  matrix<double> w = matrix<double>::Zero(n_observations, n_observations);
+  matrix<double> chols = matrix<double>::Zero(n_observations, n_observations);
+  matrix<double> phi = matrix<double>::Zero(n_observations, n_observations);
+  std::chrono::steady_clock::time_point s3 = std::chrono::steady_clock::now();
   for (int i = 0; i < n_subjects; i++) {
     int start_i = subject_zero_inds[i];
     int n_visits_i = subject_n_visits[i];
@@ -58,32 +65,43 @@ List get_empirical(List mmrm_data, NumericVector theta, NumericVector beta, Nume
       dist_i = euclidean(matrix<double>(coords.block(start_i, 0, n_visits_i, coordinates.cols())));
     }
     int subject_group_i = subject_groups[i] - 1;
-    matrix<double> sigma_inv_chol = derivatives_by_group[subject_group_i]->get_inverse_chol(visit_i, dist_i);
-    matrix<double> Xi = x_matrix.block(start_i, 0, n_visits_i, x_matrix.cols());
-    matrix<double> residual_i = residual.block(start_i, 0, n_visits_i, 1);
+    matrix<double> sigma_inv = derivatives_by_group[subject_group_i]->get_inverse(visit_i, dist_i);
     matrix<double> gi_sqrt_root = G_sqrt.segment(start_i, n_visits_i).matrix().asDiagonal();
-    matrix<double> gi_simga_inv_chol = gi_sqrt_root * sigma_inv_chol;
-    matrix<double> xt_gi_simga_inv_chol = Xi.transpose() * gi_simga_inv_chol;
-    matrix<double> identity = matrix<double>::Identity(n_visits_i, n_visits_i);
-    if (jackknife) {
-      identity = identity - xt_gi_simga_inv_chol.transpose() * beta_vcov_matrix * xt_gi_simga_inv_chol;
-    }
-    matrix<double> xta = xt_gi_simga_inv_chol * identity.inverse();
-    matrix<double> z = xta * gi_simga_inv_chol.transpose() * residual_i;
-    meat = meat + z * z.transpose();
-    xt_g_simga_inv_chol.block(0, start_i, p, n_visits_i) = xt_gi_simga_inv_chol;
-    ax.block(start_i, 0, n_visits_i, p) = xta.transpose();
+    w.block(start_i, start_i, n_visits_i, n_visits_i) = gi_sqrt_root * sigma_inv * gi_sqrt_root;
+    chols.block(start_i, start_i, n_visits_i, n_visits_i) = derivatives_by_group[subject_group_i]->get_inverse_chol(visit_i, dist_i);
+    phi.block(start_i, start_i, n_visits_i, n_visits_i) = derivatives_by_group[subject_group_i]->get_sigma(visit_i, dist_i);
   }
-  matrix<double> h = xt_g_simga_inv_chol.transpose() * beta_vcov_matrix * xt_g_simga_inv_chol;
-  matrix<double> imh = matrix<double>::Identity(n_observations, n_observations) - h;
-  matrix<double> ax_xtx =  ax * beta_vcov_matrix;
+  std::chrono::steady_clock::time_point s4 = std::chrono::steady_clock::now();
+  matrix<double> weighted_phi = matrix<double>::Zero(n_observations, n_observations);
+  weighted_phi = (1 / G_sqrt).matrix().asDiagonal() * phi * (1 / G_sqrt).matrix().asDiagonal();
+  matrix<double> hxw = hx * w; // probably slow. use block calculation!
+  matrix<double> i_hxw = matrix<double>::Identity(n_observations, n_observations) - hxw;
   matrix<double> g = matrix<double>::Zero(n_observations, p * n_subjects);
+  std::chrono::steady_clock::time_point s5 = std::chrono::steady_clock::now();
   for (int i = 0; i < n_subjects; i++) {
     int start_i = subject_zero_inds[i];
     int n_visits_i = subject_n_visits[i];
-    g.block(0, i * p, n_observations, p) = imh.block(0, start_i, n_observations, n_visits_i) * ax_xtx.block(start_i, 0, n_visits_i, p);
+    matrix<double> ai = matrix<double>::Identity(n_visits_i, n_visits_i);
+    if (type == 1) { // 1 for jackknife
+      ai = (ai - hxw.block(start_i, start_i, n_visits_i, n_visits_i)).inverse();
+    } else if (type == 2) {
+      matrix<double> di = chols.block(start_i, start_i, n_visits_i, n_visits_i);
+      matrix<double> bi1 = di * i_hxw.block(start_i, 0, n_visits_i, n_observations);
+      matrix<double> bi = bi1 * phi * bi1.transpose();
+      matrix<double> bisq = matrix<double>(bi.completeOrthogonalDecomposition().pseudoInverse()).sqrt();
+      ai = di.transpose() * bisq * di;
+    }
+    matrix<double> Xi = x_matrix.block(start_i, 0, n_visits_i, x_matrix.cols());
+    matrix<double> wi = w.block(start_i, start_i, n_visits_i, n_visits_i);
+    matrix<double> residual_i = residual.block(start_i, 0, n_visits_i, 1);
+    matrix<double> xtwa = Xi.transpose() * wi * ai;
+    matrix<double> z =  xtwa * residual_i;
+    meat = meat + z * z.transpose();
+    g.block(0, i * p, n_observations, p) = i_hxw.block(start_i, 0, n_visits_i, n_observations).transpose() * xtwa.transpose() * beta_vcov_matrix;
   }
-  matrix<double> gtvg = g.transpose() * g;
+  std::chrono::steady_clock::time_point s6 = std::chrono::steady_clock::now();
+  matrix<double> gtvg = g.transpose() * weighted_phi * g; // very large matrix calculation? also use block?
+  std::chrono::steady_clock::time_point s7 = std::chrono::steady_clock::now();
   for (int r = 0; r < n_groups; r++) {
     delete derivatives_by_group[r];
   }
@@ -94,6 +112,16 @@ List get_empirical(List mmrm_data, NumericVector theta, NumericVector beta, Nume
   //if (jackknife) {
   //  ret = ret * (n_subjects - 1) / n_subjects;
   //}
+  std::chrono::steady_clock::time_point s8 = std::chrono::steady_clock::now();
+
+  std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(s2 - s1).count() << "[µs]" << std::endl;
+  std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(s3 - s2).count() << "[µs]" << std::endl;
+  std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(s4 - s3).count() << "[µs]" << std::endl;
+  std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(s5 - s4).count() << "[µs]" << std::endl;
+  std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(s6 - s5).count() << "[µs]" << std::endl;
+  std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(s7 - s6).count() << "[µs]" << std::endl;
+  std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(s8 - s7).count() << "[µs]" << std::endl;
+
   return List::create(
     Named("cov") = as_mv(ret),
     Named("df_mat") = as_mv(gtvg)
