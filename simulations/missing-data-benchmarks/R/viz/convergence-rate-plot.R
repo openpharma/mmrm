7de7a018953ad90ab5667feaa62c5aa454905e9a
PhilBoileau <boileap2@gene.com>
2023-04-17 13:41:12 -0700

    updates to plotting functions

--- simulations/missing-data-benchmarks/R/viz/convergence-rate-plot.R
+++ simulations/missing-data-benchmarks/R/viz/convergence-rate-plot.R
@@ -1,17 +1,71 @@
 convergence_rate_plot_fun <- function(eval_results) {
 
-  eval_results$convergence_rate %>%
+  cr_tbl <- eval_results$convergence_rate %>%
+    mutate(
+      true_covar = ifelse(stringr::str_detect(.dgp_name, "_us"),
+                          "Unstructured",
+                          ifelse(stringr::str_detect(.dgp_name, "_csh"),
+                                 "Comp. Sym. (Hom.)", "Toeplitz (Hom.)")),
+      effect_size = ifelse(stringr::str_detect(.dgp_name, "no_effect"),
+                           "No Effect",
+                           ifelse(stringr::str_detect(.dgp_name, "small_effect"),
+                                  "Small Effect", "Moderate Effect")),
+      effect_size = factor(
+        effect_size, levels = c("No Effect", "Small Effect", "Moderate Effect")
+      )
+    )
+
+  unstructured_plot <- cr_tbl %>%
+    dplyr::filter(true_covar == "Unstructured") %>%
     ggplot2::ggplot(ggplot2::aes(
       x = .method_name, y = convergence_rate
     )) +
-    ggplot2::facet_grid(rows = ggplot2::vars(.dgp_name)) +
+    ggplot2::facet_grid(rows = ggplot2::vars(effect_size)) +
     ggplot2::geom_point() +
     ggplot2::xlab("Method") +
-    ggplot2::ylab("Mean Fit Time in Seconds (100 Replicates)") +
+    ggplot2::ylab("Convergence Rate (100 Replicates)") +
+    ggplot2::ggtitle("Unstructured Covariance Matrix") +
     ggplot2::scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
     ggplot2::theme_bw() +
     ggplot2::theme(
       axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust = 1)
     )
+  csh_plot <- cr_tbl %>%
+    dplyr::filter(true_covar == "Comp. Sym (Hom.)") %>%
+    ggplot2::ggplot(ggplot2::aes(
+      x = .method_name, y = convergence_rate
+    )) +
+    ggplot2::facet_grid(rows = ggplot2::vars(effect_size)) +
+    ggplot2::geom_point() +
+    ggplot2::xlab("Method") +
+    ggplot2::ylab("Convergence Rate (100 Replicates)") +
+    ggplot2::ggtitle("Homogeneous Compound Symmetry Covariance Matrix") +
+    ggplot2::scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+    ggplot2::theme_bw() +
+    ggplot2::theme(
+      axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust = 1)
+    )
+  toeph_plot <- cr_tbl %>%
+    dplyr::filter(true_covar == "Toeplitz (Hom.)") %>%
+    ggplot2::ggplot(ggplot2::aes(
+      x = .method_name, y = convergence_rate
+    )) +
+    ggplot2::facet_grid(rows = ggplot2::vars(effect_size)) +
+    ggplot2::geom_point() +
+    ggplot2::xlab("Method") +
+    ggplot2::ylab("Convergence Rate (100 Replicates)") +
+    ggplot2::ggtitle("Homogeneous Covariance Matrix") +
+    ggplot2::scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+    ggplot2::theme_bw() +
+    ggplot2::theme(
+      axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust = 1)
+    )
+
+  ggarrange(
+    plotlist = list(unstructured_plot, csh_plot, toeph_plot),
+    nrow = 3,
+    common.legend = TRUE,
+    legend = "right"
+  )
 
 }
--- simulations/missing-data-benchmarks/R/viz/variance-plot.R
+++ simulations/missing-data-benchmarks/R/viz/variance-plot.R
@@ -4,25 +4,77 @@ variance_plot_fun <- function(eval_results) {
   variance_tbl <- eval_results$variance %>%
     mutate(
       coefficient_num = as.numeric(stringr::str_extract(coefficient, "[0-9].")),
-      parameter = paste("Treatment x Visit", coefficient_num),
+      parameter = paste("Visit", coefficient_num),
       parameter = factor(parameter,
-                         levels = paste("Treatment x Visit", seq_len(10)))
+                         levels = paste("Visit", seq_len(10))),
+      true_covar = ifelse(stringr::str_detect(.dgp_name, "_us"),
+                          "Unstructured",
+                          ifelse(stringr::str_detect(.dgp_name, "_csh"),
+                                 "Comp. Sym. (Hom.)", "Toeplitz (Hom.)")),
+      effect_size = ifelse(stringr::str_detect(.dgp_name, "no_effect"),
+                           "No Effect",
+                           ifelse(stringr::str_detect(.dgp_name, "small_effect"),
+                                  "Small Effect", "Moderate Effect")),
+      effect_size = factor(
+        effect_size, levels = c("No Effect", "Small Effect", "Moderate Effect")
+      )
     )
 
-  ## plot the biases
-  variance_tbl %>%
+  ## plot the variances
+  unstructured_plot <- variance_tbl %>%
+    dplyr::filter(true_covar == "Unstructured") %>%
     ggplot2::ggplot(ggplot2::aes(
       x = parameter, y = variance, colour = .method_name
     )) +
-    ggplot2::facet_grid(rows = ggplot2::vars(.dgp_name)) +
+    ggplot2::facet_grid(rows = ggplot2::vars(effect_size), scales = "free_y") +
     ggplot2::geom_point(position = position_dodge(width = 0.7)) +
     ggplot2::geom_hline(yintercept = 0, linetype = 2, alpha = 0.3) +
-    ggplot2::xlab("Interaction Coefficient") +
+    ggplot2::xlab("Treatment Effect") +
     ggplot2::ylab("Empirical Variance (100 Replicates)") +
     ggplot2::scale_colour_discrete(name = "Method") +
+    ggplot2::ggtitle("Unstructured Covariance Matrix") +
     ggplot2::theme_bw() +
     ggplot2::theme(
       axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust = 1)
     )
+  csh_plot <- variance_tbl %>%
+    filter(true_covar == "Comp. Sym. (Hom.)") %>%
+    ggplot2::ggplot(ggplot2::aes(
+      x = parameter, y = variance, colour = .method_name
+    )) +
+    ggplot2::facet_grid(rows = ggplot2::vars(effect_size), scales = "free_y") +
+    ggplot2::geom_point(position = position_dodge(width = 0.7)) +
+    ggplot2::geom_hline(yintercept = 0, linetype = 2, alpha = 0.3) +
+    ggplot2::xlab("Treatment Effect") +
+    ggplot2::ylab("Empirical Variance (100 Replicates)") +
+    ggplot2::scale_colour_discrete(name = "Method") +
+    ggplot2::ggtitle("Homogeneous Compound Symmetry Covariance Matrix") +
+    ggplot2::theme_bw() +
+    ggplot2::theme(
+      axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust = 1)
+    )
+  toeph_plot <- variance_tbl %>%
+    filter(true_covar == "Toeplitz (Hom.)") %>%
+    ggplot2::ggplot(ggplot2::aes(
+      x = parameter, y = variance, colour = .method_name
+    )) +
+    ggplot2::facet_grid(rows = ggplot2::vars(effect_size), scales = "free_y") +
+    ggplot2::geom_point(position = position_dodge(width = 0.7)) +
+    ggplot2::geom_hline(yintercept = 0, linetype = 2, alpha = 0.3) +
+    ggplot2::xlab("Treatment Effect") +
+    ggplot2::ylab("Empirical Variance (100 Replicates)") +
+    ggplot2::scale_colour_discrete(name = "Method") +
+    ggplot2::ggtitle("Homogeneous Toeplitz Covariance Matrix") +
+    ggplot2::theme_bw() +
+    ggplot2::theme(
+      axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust = 1)
+    )
+
+  ggarrange(
+    plotlist = list(unstructured_plot, csh_plot, toeph_plot),
+    nrow = 3,
+    common.legend = TRUE,
+    legend = "right"
+  )
 
 }
