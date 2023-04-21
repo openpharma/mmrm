a138cd9bf5a620866abc39543f1a6f4463a520b3
Liming Li <liming.li@roche.com>
2023-04-21 05:38:44 +0000

    add comparison of speed

--- simulations/chols/cached_chols/main.R
+++
@@ -1,9 +0,0 @@
-# withr::with_libpaths("simulations/chols/cached_chols/library", remotes::install_github("openpharma/mmrm", ref = "232_cholesky"))
-library(mmrm, lib.loc = "simulations/chols/cached_chols/library")
-
-formula <- FEV1 ~ RACE + SEX + ARMCD * AVISIT + ad(AVISIT | USUBJID)
-
-microbenchmark::microbenchmark(
-  mmrm(formula, fev_data, reml = TRUE),
-  times = 100L
-)
