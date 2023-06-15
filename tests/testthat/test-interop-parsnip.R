fc28a813291046e89cc6e1c0f85eaf1c52eded2c
Liming Li <liming.li@roche.com>
2023-06-15 13:05:51 +0000

    style file

--- tests/testthat/test-interop-parsnip.R
+++ tests/testthat/test-interop-parsnip.R
@@ -7,7 +7,11 @@ test_that("mmrm works when using tidymodels", {
   )
   expect_silent(
     linear_reg() |>
-      set_engine("mmrm", control = mmrm_control(method = "Satterthwaite"), covariance = as.cov_struct(~ us(AVISIT | USUBJID))) |>
+      set_engine(
+        "mmrm",
+        control = mmrm_control(method = "Satterthwaite"),
+        covariance = as.cov_struct(~ us(AVISIT | USUBJID))
+      ) |>
       fit(FEV1 ~ RACE + ARMCD * AVISIT, fev_data)
   )
   expect_silent(
