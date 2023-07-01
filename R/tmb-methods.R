eb34b0310b8fd4cd40166919eeebdd7d80fd0905
Daniel Leibovitz <danleibovitz@gmail.com>
2023-07-01 15:31:29 +0200

    separate newdata from no-newdata case. For no-newdata case, simulate from fitted model. For newdata case, simulate from predict()

--- R/tmb-methods.R
+++ R/tmb-methods.R
@@ -442,39 +442,79 @@ h_residuals_response <- function(object) {
 #' @return A \code{data.frame} of dimension [n, m] where n is the number of observations
 #' fitted in the model, and m is the number \code{nsim} of simulated responses.
 simulate.mmrm_tmb <- function(object, nsim = 1,
-                          seed = NULL, newdata = object$data,
+                          seed = NULL, newdata = NULL,
                           ...){
   if(!is.null(seed)) set.seed(seed)
 
-  # build data.frame of prediction vectors
-  ret <- predict(object, newdata = newdata, se.fit = FALSE, interval = "none")
-  ret <- data.frame(replicate(nsim, ret))
-  covmat <- VarCorr(object)
-  grouped <- if(object$tmb_data$n_groups > 1) TRUE else FALSE
-  subjects <- unique(newdata$subject) # TODO
-  n_subjects <- length(subjects) # TODO
-  subject_n_visits <- apply(1:n_sujbects, 1, function(x) sum(subjects[subjects == x])) # TODO
-  subject_zero_inds <- apply(1:n_subjects, 1, function(x) first(subjects[subjects == x])) # TODO get zero inds
-  visit_zero_inds <- # TODO get visits
+  if(is.null(newdata)){
+    # build data.frame of prediction vectors
+    ret <- fitted(object)
+    ret <- data.frame(replicate(nsim, ret))
 
-  for(i in 1:n_subjects){
+    # get covariance matrix
+    covmat <- VarCorr(object)
+    grouped <- if(object$tmb_data$n_groups > 1) TRUE else FALSE
 
-    # Obtain indices of data.frame belonging to subject i
-    num_visits <- subject_n_visits[i]
-    inds <- (subject_zero_inds[i] + 1) : (subject_zero_inds[i] + num_visits)
-    visits <- visits_zero_inds[inds] + 1
+    # get dataframe values
+    n_subjects <- object$tmb_data$n_subjects
+    subject_n_visits <- object$tmb_data$subject_n_visits
+    subject_zero_inds <- object$tmb_data$subject_zero_inds
+    visit_zero_inds <- object$tmb_data$visits_zero_inds
 
-    # get relevant covariance matrix for subject i
-    covmat_i <- if(grouped){
-      covmat[[object$tmb_data$subject_groups[i]]]
-    }else{
-      covmat
+    for(i in 1:n_subjects){
+
+      # Obtain indices of data.frame belonging to subject i
+      num_visits <- subject_n_visits[i]
+      inds <- (subject_zero_inds[i] + 1) : (subject_zero_inds[i] + num_visits)
+      visits <- visits_zero_inds[inds] + 1
+
+      # get relevant covariance matrix for subject i
+      covmat_i <- if(grouped){
+        covmat[[object$tmb_data$subject_groups[i]]]
+      }else{
+        covmat
+      }
+      # subset covariance matrix
+      covmat_i <- covmat_i[visits, visits]
+
+      # simulate from covariance matrix
+      ret[inds,] <- ret[inds, , drop=FALSE] + MASS::mvrnorm(nsim, rep.int(0, num_visits), covmat_i)
     }
-    # subset covariance matrix
-    covmat_i <- covmat_i[visits, visits]
+  }else{
+    # build data.frame of prediction vectors
+    ret <- predict(object, newdata = newdata, se.fit = FALSE, interval = "none")
+    ret <- data.frame(replicate(nsim, ret))
 
-    # simulate from covariance matrix
-    ret[inds,] <- ret[inds, , drop=FALSE] + MASS::mvrnorm(nsim, rep.int(0, num_visits), covmat_i)
+    # get covariance matrix
+    covmat <- VarCorr(object)
+    grouped <- if(object$tmb_data$n_groups > 1) TRUE else FALSE
+
+    # get dataframe values
+    subjects <- unique(newdata$subject) # TODO
+    n_subjects <- length(subjects) # TODO
+    subject_n_visits <- apply(1:n_sujbects, 1, function(x) sum(subjects[subjects == x])) # TODO
+    subject_zero_inds <- apply(1:n_subjects, 1, function(x) first(subjects[subjects == x])) # TODO get zero inds
+    visit_zero_inds <- # TODO get visits
+
+      for(i in 1:n_subjects){
+
+        # Obtain indices of data.frame belonging to subject i
+        num_visits <- subject_n_visits[i]
+        inds <- (subject_zero_inds[i] + 1) : (subject_zero_inds[i] + num_visits)
+        visits <- visits_zero_inds[inds] + 1
+
+        # get relevant covariance matrix for subject i
+        covmat_i <- if(grouped){
+          covmat[[object$tmb_data$subject_groups[i]]]
+        }else{
+          covmat
+        }
+        # subset covariance matrix
+        covmat_i <- covmat_i[visits, visits]
+
+        # simulate from covariance matrix
+        ret[inds,] <- ret[inds, , drop=FALSE] + MASS::mvrnorm(nsim, rep.int(0, num_visits), covmat_i)
+      }
   }
 
   ret
