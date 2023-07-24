49c6fb6f8127429244043818fb10036e4042abeb
Daniel Leibovitz <danleibovitz@gmail.com>
2023-07-24 19:22:21 +0200

    in method = conditional, skip pover pts who already have all values observed

--- R/tmb-methods.R
+++ R/tmb-methods.R
@@ -475,7 +475,7 @@ simulate.mmrm_tmb <- function(object, nsim = 1,
   if(method == "conditional"){
     # order data.frame
     # TODO remove -- conform with predict() method approach
-    newdata <- dplyr::arrange(newdata, !!object$formula_parts$subject_var, !!object$formula_parts$visit_var)
+    # newdata <- dplyr::arrange(newdata, !!object$formula_parts$subject_var, !!object$formula_parts$visit_var)
 
     # get prediction and prediction variance
     mu <- h_get_prediction(tmb_data, object$theta_est, object$beta_est, object$beta_vcov)
@@ -491,22 +491,21 @@ simulate.mmrm_tmb <- function(object, nsim = 1,
     visit_zero_inds <- tmb_data$visits_zero_inds
 
       for(i in 1:n_subjects){
-        # TODO for pts with already filled values, are we replacing here? we should leave alone
+        if(length(mu$index[[i]]) > 0){
+          # Obtain indices of data.frame belonging to subject i (iterate by 1, since indices from cpp are 0-order)
+          inds <- mu$index[[i]] + 1
 
+          # get relevant covariance matrix for subject i
+          covmat_i <- mu$covariance[[i]]
 
-        # Obtain indices of data.frame belonging to subject i (iterate by 1, since indices from cpp are 0-order)
-        inds <- mu$index[[i]] + 1
-
-        # get relevant covariance matrix for subject i
-        covmat_i <- mu$covariance[[i]]
-
-        # simulate from covariance matrix
-        ret[inds,] <- ret[inds, , drop=FALSE] + MASS::mvrnorm(nsim, rep.int(0, length(inds)), covmat_i)
+          # simulate from covariance matrix
+          ret[inds,] <- ret[inds, , drop=FALSE] + MASS::mvrnorm(nsim, rep.int(0, length(inds)), covmat_i)
+        }
       }
   }else if(method == "marginal"){
     # order data.frame
     # TODO remove -- conform with predict() method approach
-    newdata <- dplyr::arrange(newdata, !!object$formula_parts$subject_var, !!object$formula_parts$visit_var)
+    # newdata <- dplyr::arrange(newdata, !!object$formula_parts$subject_var, !!object$formula_parts$visit_var)
 
     # build data.frame of prediction vectors
     ret <- as.data.frame(matrix(NA, ncol = nsim, nrow = nrow(newdata)))
