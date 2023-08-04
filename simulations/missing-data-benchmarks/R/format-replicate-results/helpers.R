97f92bf9a4b0bd5ba313c4a9cb8d400371354a5e
Philippe Boileau <philippe_boileau@berkeley.edu>
2023-08-04 11:50:28 -0400

    add helpers for extracting emmeans and emmeans-like fit summaries

---
+++ simulations/missing-data-benchmarks/R/format-replicate-results/helpers.R
@@ -0,0 +1,110 @@
+# compute the 95% CI from emmeans output
+get_95_ci <- function(emmeans_df) {
+  emmeans_df %>%
+    mutate(
+      lower = estimate - 1.96 * SE,
+      upper = estimate + 1.96 * SE,
+    )
+}
+
+format_emmeans_df <- function(emmeans_df) {
+  emmeans_df %>%
+    transmute(
+      visit_num = visit_num,
+      estimate = estimate,
+      stderr = SE,
+      df = df,
+      tvalue = t.ratio,
+      pvalue = p.value,
+      lower = lower,
+      upper = upper
+    )
+}
+
+# extract model fits output at each visit from mmrm fit
+get_mmrm_emmeans_output <- function(fit) {
+
+  # extract emmeans output
+  marginal_means <- emmeans(
+    fit,
+    spec = trt.vs.ctrl ~ trt | visit_num,
+    weights = "proportional"
+  )
+  emmeans_df <- as.data.frame(marginal_means$contrasts)
+
+  # compute lower and upper 95% CI
+  emmeans_df <- get_95_ci(emmeans_df)
+
+  # format to resemble SAS output
+  format_emmeans_df(emmeans_df)
+
+}
+
+# extract model fit outputs at each visit from glmmTMB fit
+get_glmm_emmeans_output <- function(fit) {
+  marginal_means <- emmeans(
+    fit,
+    spec = trt.vs.ctrl ~ trt | visit_num,
+    weights = "proportional"
+  )
+  emmeans_df <- as.data.frame(marginal_means$contrasts)
+
+  # compute lower and upper 95% CI
+  emmeans_df <- get_95_ci(emmeans_df)
+
+  # format to resemble SAS output
+  format_emmeans_df(emmeans_df)
+
+}
+
+# extract model fit outputs estimates at each visit from gls fit
+get_nlme_emmeans_output <- function(fit, data) {
+  marginal_means <- emmeans(
+    fit,
+    spec = trt.vs.ctrl ~ trt | visit_num,
+    weights = "proportional",
+    data = data,
+    mode = "df.error"
+  )
+  emmeans_df <- as.data.frame(marginal_means$contrasts)
+
+  # compute lower and upper 95% CI
+  emmeans_df <- get_95_ci(emmeans_df)
+
+  # format to resemble SAS output
+  format_emmeans_df(emmeans_df)
+
+}
+
+# extract model fit outputs estimates at each visit from PROC MIXED fit
+get_mixed_emmeans_like_output <- function(fit) {
+  fit %>% transmute(
+    visit_num = str_extract(contrast, "^([^:])+"),
+    estimate = Estimate,
+    stderr = StdErr,
+    df = DF,
+    tvalue = tValue,
+    pvalue = map2(tValue, DF, function(tvalue, df) {
+      2 * min(c(pt(tvalue, df), pt(tvalue, df, lower.tail = FALSE)))
+    }),
+    lower = Lower,
+    upper = Upper
+  )
+}
+
+# general function for emmeans like information
+get_emmeans_output <- function(method, fit, dt, converged) {
+  if (get_convergence(method, fit, converged)) {
+    if (str_detect(method, "mmrm")) {
+      get_mmrm_emmeans_output(fit)
+    } else if (str_detect(method, "glmmtmb")) {
+      get_glmm_emmeans_output(fit)
+    } else if (str_detect(method, "nlme")) {
+      get_nlme_emmeans_output(fit, dt)
+    } else if (str_detect(method, "proc_mixed")) {
+      get_mixed_emmeans_like_output(fit)
+    }
+  } else {
+    NA
+  }
+}
