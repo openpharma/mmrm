af6f3ec719267d412fcae8713c822810f8a26340
Yoni Sidi <yoni.sidi@sagerx.com>
2023-08-13 16:30:19 +0000

    - refactor mmrm_augment_newdata - update ns prefix on all imported fns - bump back pkg version

--- R/tidiers.R
+++ R/tidiers.R
@@ -25,7 +25,7 @@ generics::augment
 #' @return All tidying methods return a tibble.
 #'   The structure depends on the method chosen.
 #' @examples
-#' if(interactive()){
+#'
 #'
 #'   # fit a model
 #'     fit <- mmrm(FEV1 ~ RACE + SEX + ARMCD * AVISIT + us(AVISIT | USUBJID), data = fev_data)
@@ -45,25 +45,25 @@ generics::augment
 #'
 #'     fit |> augment(type.residuals='pearson')
 #'     fit |> augment(type.residuals='normalized')
-#'  }
+#'
 #' @seealso
-#'  \code{\link[tibble]{as_tibble}}, \code{\link[tibble]{enframe}}
+#'  [tibble::as_tibble()], [tibble::enframe()]
 #' @rdname tidiers
 #' @export
 #' @importFrom tibble as_tibble enframe
 tidy.mmrm <- function (x, conf.int = FALSE, conf.level = 0.95) {
-    ret <- summary(x)$coefficients  |> tibble::as_tibble(rownames = "term")
-    colnames(ret) <- c("term", "estimate", "std.error", "df", "statistic", "p.value")
-    coefs <- coef(x)
-        if (length(coefs) != nrow(ret)) {
-            coefs <- coefs |> tibble::enframe(name = "term", value = "estimate")
-            ret <- merge(coefs, ret, by = c("term", "estimate"))
-        }
-        if (conf.int) {
-            ci <- mmrm_confint_terms(x, level = conf.level)
-            ret <- merge(ret, ci, by = "term") |> tibble::as_tibble()
-        }
-        ret
+  ret <- summary(x)$coefficients  |> tibble::as_tibble(rownames = "term")
+  colnames(ret) <- c("term", "estimate", "std.error", "df", "statistic", "p.value")
+  coefs <- coef(x)
+  if (length(coefs) != nrow(ret)) {
+    coefs <- coefs |> tibble::enframe(name = "term", value = "estimate")
+    ret <- merge(coefs, ret, by = c("term", "estimate"))
+  }
+  if (conf.int) {
+    ci <- h_mmrm_confint_terms(x, level = conf.level)
+    ret <- merge(ret, ci, by = "term") |> tibble::as_tibble()
+  }
+  ret
 }
 
 #' @rdname tidiers
@@ -83,7 +83,7 @@ augment.mmrm <- function(x, newdata = NULL,
   .resid <- NULL
   type.residuals <- match.arg(type.residuals, choices = c("response", "pearson", "normalized"))
   if (missing(newdata)) {
-    newdata <- mmrm_getData(x)
+    newdata <- mmrm_get_data(x)
     .resid <- data.frame(.rownames = rownames(newdata), .resid = residuals(x, type = type.residuals) |> unname())
   }
   interval <- match.arg(interval, choices = c("none", "confidence", "prediction"))
@@ -101,21 +101,21 @@ augment.mmrm <- function(x, newdata = NULL,
 
 
 #' @importFrom tibble as_tibble
-mmrm_confint_terms <- function (x, ...)
+h_mmrm_confint_terms <- function (x, ...)
 {
-    ci <- suppressMessages(confint(x, ...))
-    if (is.null(dim(ci))) {
-        ci <- matrix(ci, nrow = 1)
-        rownames(ci) <- names(coef(x))[1]
-    }
-    ci <- tibble::as_tibble(ci, rownames = "term", .name_repair = "minimal")
-    names(ci) <- c("term", "conf.low", "conf.high")
-    ci
+  ci <- suppressMessages(stats::confint(x, ...))
+  if (is.null(dim(ci))) {
+    ci <- matrix(ci, nrow = 1)
+    rownames(ci) <- names(coef(x))[1]
+  }
+  ci <- tibble::as_tibble(ci, rownames = "term", .name_repair = "minimal")
+  names(ci) <- c("term", "conf.low", "conf.high")
+  ci
 }
 
 
-mmrm_getData <- function(x){
-  get_all_vars(x, data = na.omit(x$data))
+mmrm_get_data <- function(x){
+  stats::get_all_vars(x, data = stats::na.omit(x$data))
 }
 
 #' @importFrom tibble is_tibble
@@ -126,18 +126,18 @@ has_rownames <- function (df) {
   any(rownames(df) != as.character(1:nrow(df)))
 }
 
-#' @importFrom tibble add_column
+#' @importFrom tibble add_column as_tibble
 as_augment_tibble <- function (data) {
   if (inherits(data, "matrix") & is.null(colnames(data))) {
     stop("The supplied `data`/`newdata` argument was an unnamed matrix. ",
          "Please supply a matrix or dataframe with column names.")
   }
-  tryCatch(df <- as_tibble(data), error = function(cnd) {
+  tryCatch(df <- tibble::as_tibble(data), error = function(cnd) {
     stop("Could not coerce data to `tibble`. Try explicitly passing a",
          "dataset to either the `data` or `newdata` argument.",
          call. = FALSE)
   })
-  if (has_rownames(data)) {
+  if (tibble::has_rownames(data)) {
     df <- tibble::add_column(df, .rownames = rownames(data),
                              .before = TRUE)
   }
@@ -145,64 +145,47 @@ as_augment_tibble <- function (data) {
 }
 
 #' @importFrom rlang as_label f_lhs
-mmrm_augment_newdata <- function (x, data, newdata, .se_fit, interval = NULL, ...) {
+check_response <- function(xterms){
+  output <- FALSE
+
+  if (!is.null(xterms) & inherits(xterms, "formula")) {
+    output <- rlang::as_label(rlang::f_lhs(xterms)) %in%
+      names(df) || all.vars(xterms)[1] %in% names(df)
+  }
+
+  output
+}
+
+mmrm_augment_newdata <- function (x, newdata, .se_fit, interval = NULL, ...) {
+
+  df <- newdata
 
   passed_newdata <- !is.null(newdata)
-  df <- if (passed_newdata)
-    newdata
-  else data
-  df <- as_augment_tibble(df)
-  x$terms <- terms(x$formula_parts$formula)
 
-  if (!is.null(x$terms) & inherits(x$terms, "formula")) {
-    has_response <- rlang::as_label(rlang::f_lhs(x$terms)) %in%
-      names(df) || all.vars(x$terms)[1] %in% names(df)
+  if (is.null(interval)){
+    interval <- 'none'
   }
-  else {
-    has_response <- FALSE
+
+
+  if (interval == 'none'){
+    .se_fit <- FALSE
   }
+
+  df <- as_augment_tibble(df)
+  has_response <- check_response(stats::terms(x$formula_parts$formula))
+  pred_obj <- predict(x, newdata = newdata, na.action = na.pass, se.fit = .se_fit, interval = interval, ...)
+
+  if(interval=='none'){
+    df$.fitted <- pred_obj |> unname()
+  }else{
+    df$.fitted <- pred_obj[, "fit"]
+    df$.lower  <- pred_obj[, "lwr"]
+    df$.upper  <- pred_obj[, "upr"]
+  }
+
   if (.se_fit) {
-    pred_obj <- predict(x, newdata = newdata, na.action = na.pass,
-                        se.fit = .se_fit, interval = interval, ...)
-    if (is.null(interval) || interval == "none") {
-      df$.fitted <- pred_obj$fit |> unname()
-    }
-    else {
-      df$.fitted <- pred_obj[, "fit"]
-      df$.lower <- pred_obj[, "lwr"]
-      df$.upper <- pred_obj[, "upr"]
-    }
     se_idx <- which(colnames(pred_obj) %in% c("se.fit", "se"))
     df$.se.fit <- pred_obj[[se_idx]]
-  }
-  else if (!is.null(interval) && interval != "none") {
-    pred_obj <- predict(x, newdata = newdata, na.action = na.pass, se.fit = .se_fit, interval = interval, ...)
-    df$.fitted <- pred_obj[, "fit"]
-    df$.lower <- pred_obj[, "lwr"]
-    df$.upper <- pred_obj[, "upr"]
-  }
-  else if (passed_newdata) {
-    if (is.null(interval) || interval == "none") {
-
-      df$.fitted <- predict(x, newdata = newdata, na.action = na.pass, se.fit = FALSE, ...) |> unname()
-    }
-    else {
-      pred_obj <- predict(x, newdata = newdata, na.action = na.pass, interval = interval, se.fit = .se_fit, ...)
-      df$.fitted <- pred_obj[, "fit"]
-      df$.lower <- pred_obj[, "lwr"]
-      df$.upper <- pred_obj[, "upr"]
-    }
-  }
-  else {
-    if (is.null(interval) || interval == "none") {
-      df$.fitted <- predict(x, na.action = na.pass, ...) |> unname()
-    }
-    else {
-      pred_obj <- predict(x, newdata = newdata, na.action = na.pass, interval = interval, se.fit = .se_fit, ...)
-      df$.fitted <- pred_obj[, "fit"]
-      df$.lower <- pred_obj[, "lwr"]
-      df$.upper <- pred_obj[, "upr"]
-    }
   }
 
   df
