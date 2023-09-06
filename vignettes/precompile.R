c95a82ba9eeec88c3bd5896f0077c93150d1285e
Liming Li <liming.li@roche.com>
2023-09-06 04:52:03 +0000

    update vignettes

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,6 +1,6 @@
 # Precompile vignettes which are too slow.
 # Must compile and install the `mmrm` package before knitting the vignette.
-
+library(mmrm)
 library(knitr)
 
 knit("vignettes/mmrm_review_methods.Rmd.orig", output = "vignettes/mmrm_review_methods.Rmd")
