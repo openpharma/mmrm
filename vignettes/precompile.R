fcc15b92a59032246c1cbb4ca27bc722fc96f13d
Liming <36079400+clarkliming@users.noreply.github.com>
2023-09-18 21:43:08 +0800

    use glimmix instead of mixed (#349)

--- vignettes/precompile.R
+++
@@ -1,7 +0,0 @@
-# Precompile vignettes which are too slow.
-# Must compile and install the `mmrm` package before knitting the vignette.
-library(mmrm)
-library(knitr)
-
-knit("vignettes/mmrm_review_methods.Rmd.orig", output = "vignettes/mmrm_review_methods.Rmd")
-system("mv *png vignettes/")
