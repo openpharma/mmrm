40d8d5cf4fa996a371a9ca9e9ee05315c1a520c2
Daniel Sabanes Bove <daniel.sabanesbove@gmx.net>
2025-09-09 16:02:11 +0800

    adding a test for this... but not sure we really want that

--- tests/testthat/test-car.R
+++ tests/testthat/test-car.R
@@ -300,6 +300,22 @@ test_that("Anova works if covariate are character", {
   )
 })
 
+test_that("Anova fails as expected when a contrast other than contr.treatment is used", {
+  skip_if_not_installed("car")
+  withr::local_options(
+    list(contrasts = c("contr.sum", "contr.poly"))
+  )
+  fit <- mmrm(FEV1 ~ ARMCD * AVISIT + ar1(AVISIT | USUBJID), data = fev_data)
+  expect_error(
+    Anova(fit, "III"),
+    "currently only supports treatment contrasts"
+  )
+  expect_error(
+    Anova(fit, "II"),
+    "currently only supports treatment contrasts"
+  )
+})
+
 # Anova Integration Test ----
 
 test_that("Anova give similar results to SAS", {
